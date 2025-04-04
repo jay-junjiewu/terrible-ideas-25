60102f851d013c141dcbd15b2b7b004ca5d689fd
Vadim <iroQQuois@gmail.com>
2025-04-05 10:03:50 +1100

    init

---
+++ src/main/java/com/au/iroqquois/terrible_messsanger/TerribleMesssangerApplication.java
@@ -0,0 +1,13 @@
+package com.au.iroqquois.terrible_messsanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerribleMesssangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerribleMesssangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/au/iroqquois/terrible_messsanger/TerribleMesssangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.iroqquois.terrible_messsanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerribleMesssangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
